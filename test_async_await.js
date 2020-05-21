628d201563e472dd2258fc8c13d261e62a6d4b0f
FooJamFung <gzfujianfeng@corp.netease.com>
2020-05-21 10:00:17 +0800

    add async

--- test_async_await.js
+++ test_async_await.js
@@ -1,3 +1,21 @@
 /*
-go
+simple async
 */
+async function testAsync1(){
+    setTimeout(() => {
+        console.log("in async func")
+    }, 3000);
+    return "done in async func"
+}
+let a1 = testAsync1();
+a1.then(value => {
+    console.log(value);
+}).catch(err => {
+    console.error(err);
+});
+console.log("out async func");
+
+/*
+with await
+*/
+
