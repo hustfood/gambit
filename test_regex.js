2760457584805fb219372c19f969e0ab513d5847
FooJamFung <gzfujianfeng@corp.netease.com>
2020-07-30 17:51:34 +0800

    add regex sample

--- test_regex.js
+++ test_regex.js
@@ -2,7 +2,7 @@
 extract string
 */
 function testRegex1(){
-    let reg = /\W*class\W([a-zA-Z0-9]+)\(object\):$/;
+    const reg = /\W*class\W([a-zA-Z0-9]+)\(object\):$/;
     let s = "    class State(object):";
     let t = reg.exec(s);
     if (t) {
@@ -12,3 +12,19 @@ function testRegex1(){
     }
 }
 testRegex1();
+
+/*
+replace string
+*/
+function testRegex2(){
+    const reg = /http(s)?:\/\/[a-z0-9\.\-_:]+\/[a-z0-9\-_\/]+/g;
+    let s = "qwerasdfhttps://electron-react-boilerplate.js.org/docs/adding-assets/ASDFSDF234sdf";
+    let t = reg.exec(s);
+    if (t) {
+        let n_t = `<a>${t[0]}</a>`;
+        console.log(s.replace(reg, n_t));
+    } else {
+        console.log('no match')
+    }
+}
+testRegex2();
